dda95bbc5191f28fed3d091dd2dad29e8b8e3544
whwqs <whwqs@hotmail.com>
2019-06-11 23:22:08 +0800

    import（）

--- vue02/hello1/6_3.js
+++ vue02/hello1/6_3.js
@@ -1,1 +1,10 @@
-alert(800)
+alert(888)
+if(true) import('./6_4.js').then(function(m){
+    console.info("m:")
+    console.info(m)
+    console.info(m.default)
+    console.info(m.a)
+    console.info(m.b)
+}).catch(function(e){
+    console.info(e)
+})
---
+++ vue02/hello1/6_4.js
@@ -0,0 +1,4 @@
+export var a=200
+export var b='wwww'
+export default 123
+a++
