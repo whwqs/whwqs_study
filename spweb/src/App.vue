644a6f9bbc5a87df5e13fd0e8d1b78dfc2871b15
whwqs <whwqs@hotmail.com>
2019-07-10 09:15:01 +0800

    spweb\.env.development

--- spweb/src/App.vue
+++ spweb/src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld :msg="env"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+
 export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  data(){
+    return {env:"process.env.ENV"}
   }
 }
 </script>
--- spweb/src/main.js
+++ spweb/src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Object.keys({a:1,b:2}).forEach(key=>console.info(key))
+Object.values({a:1}).forEach(v=>console.info(v))
+
+
 new Vue({
-  render: h => h(App),
+  render: h => h("H1",[process.env.NODE_ENV]),
 }).$mount('#app')
